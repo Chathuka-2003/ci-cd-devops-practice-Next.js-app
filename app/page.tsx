5e109498a95e05bc11259788c4865d574cbd0a36
Chathuka-2003 <chathukapriyankaraedirisinghe@gmail.com>
2026-09-19 00:18:40 +0530

    update page.tsx with dev

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <p className="text-3xl font-bold underline text-center mt-10">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <p className="text-3xl font-bold underline text-center mt-10">
       DevOps Project new version 2.0 
       Welcome to my DevOps project! This project is designed to showcase the latest features and improvements in our application. We have implemented new functionalities, enhanced performance, and improved user experience. Stay tuned for more updates and exciting developments in the future!
-    </p>
+      </p>
+    </div>
   );
 }
